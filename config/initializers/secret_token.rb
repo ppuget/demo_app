488da8c29b726bc8a76e18a0a5d340500a4ab433
ppuget <pugetp@gmail.com>
2013-05-21 16:43:05 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = '6046dffe0815f7ceeec28c8cfb4d1e46294d77e749c3c6a05cc2c98a7ecf5f7ff0f5587b8ab7902ccb61dc9cb5db19641613f214700303e9cd8d25ab760ab044'
